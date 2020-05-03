68d31b2972ad1dad4a2ad9ff7d8ef5b726c147ac
AWhiteFox <ignat.k.150@gmail.com>
2020-05-03 17:25:13 +0300

    Add the discord name length limit handling

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 async def set_prefix(member, prefix):
-    await member.edit(nick=f'[{prefix}] {member.display_name}')
+    await member.edit(nick=f'[{prefix}] {member.display_name}'[:32])  # 32 is the discord name limit
 
 
 async def remove_prefix(member):
