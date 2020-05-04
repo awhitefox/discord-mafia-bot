f3d1a1e50f145a511578d58695021d2d6bb70973
AWhiteFox <ignat.k.150@gmail.com>
2020-05-04 15:40:57 +0300

    Redo spectator muting system
    Old system was based on server mute which cannot be removed if member left the voice channel.
    New system is based on permissions and roles.

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ guild_id = int(os.environ['GUILD'])
 
 current_voice = None
 current_players = []
+current_role = None
 
 
 @bot.check
@@ -37,16 +38,14 @@ async def on_voice_state_update(member, before, after):
     if current_voice is not None and before.channel != current_voice and after.channel == current_voice:
         if member in current_players:
             i = current_players.index(member)
-            prefix = str(i) if i != 0 else 'В'
+            prefix = f'{i:02d}' if i != 0 else 'В'
             await helpers.set_prefix(member, prefix)
         else:
             await helpers.set_prefix(member, 'Н')
-            await member.edit(mute=True)
 
     # If user left current voice
     elif before.channel == current_voice and after.channel != current_voice:
         await helpers.remove_prefix(member)
-        await member.edit(mute=False)
 
 
 # Commands
@@ -59,40 +58,53 @@ async def ping(ctx):
 @bot.command()
 @checks.voice_only()
 async def start(ctx):
-    global current_voice, current_players
+    global current_voice, current_role
     if current_voice is not None:
         await ctx.send('Игра уже запущена')
         return
     current_voice = ctx.author.voice.channel
 
-    # Put everybody in voice channel except bots and message author
+    # Put everybody in voice channel except bots and message author in current_players
     current_players.extend(filter(lambda x: not (x.bot or x == ctx.author), current_voice.members))
     # Shuffle players and put author on first place
     random.shuffle(current_players)
     current_players.insert(0, ctx.author)
 
-    await ctx.send(f'Игра начинается в **{current_voice}**! Ведуший - {ctx.author.mention}')
+    # Disallow @everyone to speak in current channel
+    await current_voice.set_permissions(ctx.guild.default_role, speak=False)
+    # Create role for player and allow them to speak
+    current_role = await ctx.guild.create_role(name='Игрок')
+    await current_voice.set_permissions(current_role, speak=True)
+    for p in current_players:
+        await p.add_roles(current_role)
+
+    await ctx.send(f'Игра начинается в **{current_voice}**! Ведущий - {ctx.author.mention}')
 
     await helpers.set_prefix(ctx.author, 'В')
     for i in range(1, len(current_players)):
-        await helpers.set_prefix(current_players[i], str(i))
+        await helpers.set_prefix(current_players[i], f'{i:02d}')
 
 
 @bot.command()
 @commands.guild_only()
 async def finish(ctx):
-    global current_voice
+    global current_voice, current_role
     if current_voice is None:
         await ctx.send('Игра не запущена')
         return
 
+    # Remove prefixes
     for p in current_voice.members:
         await helpers.remove_prefix(p)
-        await p.edit(mute=False)
+    # Allow @everybody to speak and delete current_role
+    await current_voice.set_permissions(ctx.guild.default_role, speak=None)
+    await current_role.delete()
+
     await ctx.send(f'Игра в **{current_voice}** завершена')
 
     current_voice = None
     current_players.clear()
+    current_role = None
 
 
 # Commands for debugging
