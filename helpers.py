c87542eb05574748f1e2c5db7299ccb2d6b9085e
AWhiteFox <ignat.k.150@gmail.com>
2020-05-04 16:41:07 +0300

    Improve prefix system

--- helpers.py
+++ helpers.py
@@ -1,8 +1,13 @@
-async def set_prefix(member, prefix):
+import discord
+changed_names = dict()
+
+
+async def set_prefix(member: discord.Member, prefix):
+    changed_names[member.id] = member.display_name
     await member.edit(nick=f'[{prefix}] {member.display_name}'[:32])  # 32 is the discord name limit
 
 
 async def remove_prefix(member):
-    if member.display_name[0] != '[':
+    if member.id not in changed_names:
         return
-    await member.edit(nick=member.display_name.split('] ', 1)[1])
+    await member.edit(nick=changed_names.pop(member.id))
